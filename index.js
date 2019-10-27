bcf626b60aac5843ad14694b4f7dec9f07300535
Figroc Chen <figroc@gmail.com>
2019-10-27 15:10:48 +0800

    publishing script

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const proto = require("@grpc/proto-loader");
 
 const def = { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true,
               includeDirs: [path.join(__dirname, "proto")] };
-const dir = path.join(__dirname, "proto/tensorflow_serving/apis")
+const dir = path.join(__dirname, "proto/tensorflow_serving/apis");
 const api = fs.readdirSync(dir)
               .filter(f => f.endsWith("_service.proto"))
               .map(f => path.join(dir, f));
-module.exports = grpc.loadPackageDefinition(proto.loadSync(api, def))
+module.exports = grpc.loadPackageDefinition(proto.loadSync(api, def));
