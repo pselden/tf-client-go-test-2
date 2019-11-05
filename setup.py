d368c851a0e0c9502be1e181c70f3513f8bab2bb
Figroc Chen <figroc@gmail.com>
2019-11-06 02:09:09 +0800

    build env naming

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import find_packages, setup
 
 
 setup(
-  name=environ["TFSCLIENT_DIST_NAME"],
-  version=environ["TFSCLIENT_DIST_VERSION"],
-  packages=find_packages(environ["TFSCLIENT_DIST_SOURCE"]),
-  package_dir={"": environ["TFSCLIENT_DIST_SOURCE"]},
+  name=environ["TFSCLIENT_NAME_"],
+  version=environ["TFSCLIENT_VERSION"],
+  packages=find_packages(environ["TFSCLIENT_SOURCE"]),
+  package_dir={"": environ["TFSCLIENT_SOURCE"]},
   python_requires=">=3.5",
   install_requires=["grpcio", "protobuf"],
   platforms=["any"],
-  author=environ["TFSCLIENT_DIST_AUTHOR_NAME"],
-  author_email=environ["TFSCLIENT_DIST_AUTHOR_EMAIL"],
-  license=environ["TFSCLIENT_DIST_LICENSE"],
-  url=environ["TFSCLIENT_DIST_URL"],
-  description=environ["TFSCLIENT_DIST_DESCRIPTION"],
+  author=environ["TFSCLIENT_AUTHOR_NAME"],
+  author_email=environ["TFSCLIENT_AUTHOR_EMAIL"],
+  license=environ["TFSCLIENT_LICENSE"],
+  url=environ["TFSCLIENT_URL"],
+  description=environ["TFSCLIENT_DESCRIPTION"],
   long_description=("This library does not coexist with tensorflow, "
                     "tensorflow-serving and tensorflow-serving-api. "
                     "The official tensorflow-serving-api requires package "
