4bdfa8de6c82fd7bbe09285d4599e315ef7b1186
Figroc Chen <figroc@gmail.com>
2019-11-18 19:35:53 +0800

    flexible finding grpc

--- Config.cmake
+++ Config.cmake
@@ -1,8 +1,11 @@
-find_package(PkgConfig REQUIRED)
-pkg_search_module(GRPC REQUIRED grpc)
-pkg_search_module(GRPCPP REQUIRED grpc++>=@TFSCLIENT_VERSION_GRPC@)
-
 include(CMakeFindDependencyMacro)
 find_dependency(Protobuf @TFSCLIENT_VERSION_PROTO@ REQUIRED)
+
+find_dependency(gRPC @TFSCLIENT_VERSOIN_GRPC@ CONFIG)
+if(NOT gRPC_FOUND)
+  find_package(PkgConfig REQUIRED)
+  pkg_search_module(GRPC REQUIRED grpc)
+  pkg_search_module(GRPCPP REQUIRED grpc++>=@TFSCLIENT_VERSION_GRPC@)
+endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/@TFSCLIENT_TARGETF@")
